e7aa0adbf512a0704f3bf537301437b47de9ef4e
MakovM <makovinskiy09@gmail.com>
2025-09-09 19:45:38 +0300

    Edited permissions

--- cinema/views.py
+++ cinema/views.py
@@ -159,7 +159,12 @@ class OrderViewSet(
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [IsAuthenticated()]
+        return super().get_permissions()
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
